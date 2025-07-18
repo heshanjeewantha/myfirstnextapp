9c6770fa74c54b77c5aa3a3fa7a598a307842ef4
it22578532 <heshanjeewantha@gmail.com>
2025-07-18 22:19:15 +0530

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to My First Next.js App!</h1>
+     
+    </div>
+  );
+}
