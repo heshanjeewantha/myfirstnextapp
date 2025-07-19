98f6ae5349b0eda0ff9937c845bee1deafdd9cf7
it22578532 <heshanjeewantha@gmail.com>
2025-07-19 22:06:24 +0530

    image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images:{
+   domains: ["ai.google.dev", "https://ai.google.dev/"],
+
+  }
+
+};
 
 export default nextConfig;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
+import img1 from "../../public/next.svg";
 
 export default function Home() {
   return (
@@ -30,6 +31,17 @@ export default function Home() {
       </ul>
 
         <Link href="https://google.com" target="_blank" rel="noopener noreferrer" id={styles.specialLink}>Google</Link>
+
+       <div>
+        <Image
+           src="https://ai.google.dev/static/gemini-api/docs/images/flying-pig.png" 
+           alt="Vercel Logo"
+           width={500}
+           height={500}
+        />
+        
+        </div>
+        <Image src={img1} alt="Vercel Logo" width={100} height={24} />
     </div>
   );
 }
